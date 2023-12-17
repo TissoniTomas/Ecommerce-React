1138e2b5eff456e65d328f1111ccf8c8f97efde0
TissoniTomas <tissonitomas5@gmail.com>
2023-12-17 15:18:23 -0300

    agregamos imagenes

--- proyectoReact23/src/components/navBar/navBar.jsx
+++ proyectoReact23/src/components/navBar/navBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import logo from "../../assets/Logo/logo.png"
 
 const navBar = () => {
   return (
-    <nav id='navbar' className='flex flex-row-reverse'>
+    <nav id='navbar' className='flex'>
       <a href= "../../../index.html"><img className='navbar-logo' src={logo} /></a>
       <ul id='navbar-list'>
         <li className='text-yellow-600'>HOME</li>
